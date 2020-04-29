5436205c405adce2a884257f8141838fca16dd85
Francisco <lazarojuarez.pa@gmail.com>
2020-04-29 16:36:28 -0500

    agregando el index

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="css/bootstrap.min.css" rel="stylesheet"> 
+  <LINK REL="stylesheet" TYPE="text/css" HREF="estilo_index.css">
+</head>
+ 
+<body>
+ 
+<div class="container">
+
+ 
+    <div class="login-wrap">
+        <div class="login-html">
+          <div class="login-form">
+            <div class="sign-in-htm">
+             
+              <br></br> 
+              <br></br>
+              <br></br>
+              <br></br>
+             <div class="modal-footer">
+               <a href="#" class="boton verde">JUGAR</a>
+               <a href="#" class="boton rojo">SALIR</a>
+             </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</div>
+</body>
+</html>
