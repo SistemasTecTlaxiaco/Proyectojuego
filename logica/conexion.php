0b40a784bbc0c67b34cf71fa7147ca5149e1864d
Francisco <lazarojuarez.pa@gmail.com>
2020-06-04 00:56:34 -0500

    agregando carpeta con la logica

---
+++ logica/conexion.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+   $host = "localhost";
+   $user = "root";
+   $password = "pancho";
+   $bd = "mi_proyecto1";
+
+   $conexion = mysqli_connect($host,$user,$password,$bd);
+
+   
+
+  
+
+
+
+
+?>
