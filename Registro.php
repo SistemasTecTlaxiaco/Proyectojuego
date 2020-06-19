33c1f784b9630f583e191cd354144f0cbac614bd
alejan <alejandroreyes.1f@gmail.com>
2020-06-18 20:28:03 -0500

    Agregando archivo modificado Registro.php

--- Registro.php
+++ Registro.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -6,6 +5,43 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <LINK REL="stylesheet" TYPE="text/css" HREF="estilo_registro.css">
 </head>
+<script>
+function validar(){
+	
+	if(!(/^[a-zA-ZàáâäãåacceèéêëeiìíîïlnòóôöõøùúûüuuÿýzzñçcšžÀÁÂÄÃÅACCEEÈÉÊËÌÍÎÏILNÒÓÔÖÕØÙÚÛÜUUŸÝZZÑßÇŒÆCŠŽ?ð ,.'-]{2,48}$/i.test(document.getElementById("user").value)))
+	{
+			alert('Nombre Incorrecto');
+			document.getElementById("user").style.backgroundColor='red';
+			document.getElementById("user").focus();
+			return false;
+		}else{
+  if(!(/^[a-zA-ZàáâäãåacceèéêëeiìíîïlnòóôöõøùúûüuuÿýzzñçcšžÀÁÂÄÃÅACCEEÈÉÊËÌÍÎÏILNÒÓÔÖÕØÙÚÛÜUUŸÝZZÑßÇŒÆCŠŽ?ð ,.'-]{2,48}$/i.test(document.getElementById("ap").value)))
+	{
+			alert('Apellido Paterno Incorrecto');
+			document.getElementById("ap").style.backgroundColor='red';
+			document.getElementById("ap").focus();
+			return false;
+		}else{
+  if(!(/^[a-zA-ZàáâäãåacceèéêëeiìíîïlnòóôöõøùúûüuuÿýzzñçcšžÀÁÂÄÃÅACCEEÈÉÊËÌÍÎÏILNÒÓÔÖÕØÙÚÛÜUUŸÝZZÑßÇŒÆCŠŽ?ð ,.'-]{2,48}$/i.test(document.getElementById("am").value)))
+	{
+			alert('Apellido Materno Incorrecto');
+			document.getElementById("am").style.backgroundColor='red';
+			document.getElementById("am").focus();
+			return false;
+		}else{
+       if(!(/^[0-9]{1,2}$/.test(document.getElementById("ed").value)))
+	{
+	        alert('Edad incorrecta');
+			document.getElementById("ed").style.backgroundColor='red';
+			document.getElementById("ed").focus();
+			return false;
+  }}}}}
+
+
+</script>
+
+
+
  
 <body>
  
@@ -19,28 +55,28 @@
           <div class="login-form">
             <div class="sign-in-htm">
 
-            <form action="logica/registrar.php" method="POST">
+            <form action="logica/registrar.php" onsubmit="validar();" method="POST">
               <div class="group">
                   
                  <br></br> 
                  <br></br> 
                 <label for="user" class="label">nombre</label>
-                <input id="user" name="nombre" type="text" class="input">
+                <input id="user" name="nombre" type="text" class="input" required>
               </div>
               <div class="group">
                 <label for="pass" class="label">Apellido Paterno</label>
-                <input id="ap" name="Ap" type="text" class="input" >
+                <input id="ap" name="Ap" type="text" class="input" required>
               </div>
               <div class="group">
                   <label for="pass" class="label">Apellido Materno</label>
-                  <input id="am" name="Am" type="text" class="input" >
+                  <input id="am" name="Am" type="text" class="input" requird >
                 </div>
                 <div class="group">
                   <label for="pass" class="label">Edad</label>
-                  <input id="ed" name="edad" type="number" class="input">
+                  <input id="ed" name="edad" type="number" class="input" required>
                 </div>
                 <div class="modal-footer">
-                <button id="envio" class="boton verde" >Guardar</button>
+                <button id="envio" class="boton verde" onclick="validar();" class="submit">Guardar</button>
                   <a href="inicio.php" class="boton azul">CANCELAR</a>
                 </div>
                 </form>
