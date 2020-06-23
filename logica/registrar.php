ba12f4bd1bebfb6215c4098082391d5be6107adc
Francisco <lazarojuarez.pa@gmail.com>
2020-06-23 18:23:13 -0500

    estableciendo conexion con postgress

--- logica/registrar.php
+++ logica/registrar.php
@@ -1,16 +1,15 @@
 <?php
 
 require 'conexion.php';
-include ('logica/conexion.php'):
 
 $nombre = $_POST['nombre'];
 $Ap = $_POST['Ap'];
 $Am = $_POST['Am'];
 $edad = $_POST['edad'];
 
-$query = $pdo->query("INSERT INTO public.alumnos(Nombre_alumno,Ap_paterno,Ap_materno,Edad) VALUES('$nombre','$Ap','$Am','$edad')");
+$query = "INSERT INTO public.alumnos(Nombre_alumno,Ap_paterno,Ap_materno,Edad) VALUES('$nombre','$Ap','$Am','$edad')";
 
-if ($query==TRUE){
+if (mysqli_query($conexion, $query)){
     echo '<script language="javascript">alert("Alumno agregado");window.location.href="../Registro.php"</script>';
     } else {
     echo '<script language="javascript">alert("Error al agregar el alumno");window.location.href="../Registro.php"</script>';
