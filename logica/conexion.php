7b939195c98d22a326bf79a496374125cba2339d
Francisco <lazarojuarez.pa@gmail.com>
2020-06-23 16:59:49 -0500

    estableciendo conexion con postgress

--- logica/conexion.php
+++ logica/conexion.php
@@ -1,16 +1,15 @@
+  
 <?php
 
+$host = "ec2-35-173-94-156.compute-1.amazonaws.com";
+$user = "mvlidosdeycbmw";
+$password = "dd04ab8b36a634960ab8354aef3159cb100cd52cabdb4f48757b0230bc0397d3";
+$dbname = "d5ku9iavksiii9";
+$port = "5432";
+
+  //Set DSN data source name
+    $conexion = "pgsql:host=" . $host . ";port=" . $port .";dbname=" . $dbname . ";user=" . $user . ";password=" . $password . ";";
 
 
   
-
-   $conexion = pg_connect("host=ec2-35-173-94-156.compute-1.amazonaws.com dbname=d5ku9iavksiii9 user=mvlidosdeycbmw  password=dd04ab8b36a634960ab8354aef3159cb100cd52cabdb4f48757b0230bc0397d3 ");
-
-   
-
-  
-
-
-
-
-?>
+  ?>
