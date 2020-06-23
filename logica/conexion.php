ac7cd1bafbc3e9f4b862b640a9e5dcb59ff1b17f
Francisco <lazarojuarez.pa@gmail.com>
2020-06-22 21:58:22 -0500

    estableciendo conexion con postgress

--- logica/conexion.php
+++ logica/conexion.php
@@ -2,12 +2,12 @@
 
 
 
-   $host = "localhost";
-   $user = "root";
-   $password = "pancho";
-   $bd = "mi_proyecto1";
+   $host = "ec2-35-173-94-156.compute-1.amazonaws.com";
+   $user = "mvlidosdeycbmw";
+   $password = "dd04ab8b36a634960ab8354aef3159cb100cd52cabdb4f48757b0230bc0397d3";
+   $bd = "d5ku9iavksiii9";
 
-   $conexion = mysqli_connect($host,$user,$password,$bd);
+   $conexion = pg_connect($host,$user,$password,$bd);
 
    
 
