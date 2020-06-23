bfc73a3d6919cc0b7b48392671469155d517442c
Francisco <lazarojuarez.pa@gmail.com>
2020-06-22 22:13:21 -0500

    estableciendo conexion con postgress

--- logica/conexion.php
+++ logica/conexion.php
@@ -4,7 +4,7 @@
 
   
 
-   $conexion = pg_connect("mvlidosdeycbmw : dd04ab8b36a634960ab8354aef3159cb100cd52cabdb4f48757b0230bc0397d3 @ ec2-35-173-94-156.compute-1.amazonaws.com : 5432 / d5ku9iavksiii9");
+   $conexion = pg_connect("host=ec2-35-173-94-156.compute-1.amazonaws.com dbname=d5ku9iavksiii9 user=mvlidosdeycbmw  password=dd04ab8b36a634960ab8354aef3159cb100cd52cabdb4f48757b0230bc0397d3 ");
 
    
 
