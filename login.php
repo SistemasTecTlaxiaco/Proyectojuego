f30b92ffd0c282e8135044db4954472b93e7079a
Francisco <lazarojuarez.pa@gmail.com>
2020-06-24 23:42:34 -0500

    validando login

--- login.php
+++ login.php
@@ -1,12 +1,28 @@
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <LINK REL="stylesheet" TYPE="text/css" HREF="estilo_login.css">
 </head>
- 
+<script>
+function validar(){
+  
+  if(!(/^[a-zA-ZàáâäãåacceèéêëeiìíîïlnòóôöõøùúûüuuÿýzzñçcšžÀÁÂÄÃÅACCEEÈÉÊËÌÍÎÏILNÒÓÔÖÕØÙÚÛÜUUŸÝZZÑßÇŒÆCŠŽ?ð ,.'-]{2,48}$/i.test(document.getElementById("user").value)))
+  {
+      alert('Nombre Incorrecto');
+      document.getElementById("user").style.backgroundColor='red';
+      document.getElementById("user").focus();
+      return false;
+    }else{
+  if(!(/^[a-zA-ZàáâäãåacceèéêëeiìíîïlnòóôöõøùúûüuuÿýzzñçcšžÀÁÂÄÃÅACCEEÈÉÊËÌÍÎÏILNÒÓÔÖÕØÙÚÛÜUUŸÝZZÑßÇŒÆCŠŽ?ð ,.'-]{2,48}$/i.test(document.getElementById("ap").value)))
+  {
+      alert('Apellido Incorrecto');
+      document.getElementById("ap").style.backgroundColor='red';
+      document.getElementById("ap").focus();
+      return false;
+  }}}
+  </script>
 <body>
  
 <div class="container">
@@ -17,7 +33,7 @@
           
           <div class="login-form">
             <div class="sign-in-htm">
-            <form action="logica/loguear.php" method="POST">
+            <form action="logica/loguear.php" onsubmit="validar();" method="POST" >
               <div class="group">
                   
                  <br></br> 
@@ -26,16 +42,16 @@
                  <br></br>
                   
                 <label for="user" class="label">nombre</label>
-                <input id="user"  name="usuario" type="text" class="input">
+                <input id="user"  name="usuario" type="text" class="input" required>
               </div>
               <div class="group">
                 <label for="pass" class="label">Apellido </label>
-                <input id="pass" name="clave" type="password" class="input" data-type="password">
+                <input id="pass" name="clave" type="password" class="input" id="ap" data-type="password" required>
               </div>
              
                 
                 <div class="modal-footer">
-                  <button id="envio" class="boton azul">Iniciar</button>
+                  <button id="envio" class="boton azul" onclick="validar();" class="submit">Iniciar</button>
                   <a href="inicio.php" class="boton rojo">Cancelar</a>
                 </div>
                 </form>
