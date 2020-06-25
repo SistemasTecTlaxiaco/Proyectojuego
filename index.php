c0709fc4f2f6d9b74c47e1c12d02a770dfcc6326
Francisco <lazarojuarez.pa@gmail.com>
2020-06-24 23:02:03 -0500

    ajustando imagen

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
-	<meta charset="utf-8">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="css/bootstrap.min.css" rel="stylesheet"> 
   <LINK REL="stylesheet" TYPE="text/css" HREF="estilo_index.css">
