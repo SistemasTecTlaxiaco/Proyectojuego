9aeef44e74a0dbfa3dbffe1bfd8d6e1ddd6b2bc2
alejan <alejandroreyes.1f@gmail.com>
2020-11-07 15:00:36 -0600

    archivo actualizado

--- logica/registrar.php
+++ logica/registrar.php
@@ -1,21 +1,30 @@
 <?php
 
-require_once 'conexion.php';
+require 'conexion.php';
 
+$Nombre_alumno = $_POST['Nombre_alumno'];
+$Ap_paterno= $_POST['Ap_paterno'];
+$Ap_materno = $_POST['Ap_materno'];
+$Edad = $_POST['Edad'];
 
-$nombre = $_POST['nombre'];
-$Ap = $_POST['Ap'];
-$Am = $_POST['Am'];
-$edad = $_POST['edad'];
+$query = "INSERT INTO alumnos(Nombre_alumno,Ap_paterno,Ap_materno,Edad) 
+VALUES('$Nombre_alumno','$Ap_paterno','$Ap_materno','$Edad')";
 
-$query ="INSERT INTO public.alumnos(nombre_alumno, ap_paterno, ap_materno, grado) VALUES ( '$nombre', '$Ap', '$Am', '$edad')";
-$resul=pg_query($conexion, $query);
-if ($resul){
-    echo '<script language="javascript">alert("Alumno agregado");window.location.href="../Registro.php"</script>';
-    } else {
-    echo '<script language="javascript">alert("Error al agregar el alumno");window.location.href="../Registro.php"</script>';
-    }
+$verificardup = mysqli_query($conexion, "SELECT * FROM alumnos WHERE Nombre_alumno='$Nombre_alumno' AND Ap_paterno='$Ap_paterno' AND Ap_materno='$Ap_materno'");
 
+if(mysqli_num_rows($verificardup)>0){
+    
+    echo '<script language="javascript">
+    alert("El registro ya existe, intente de nuevo");window.location.href="../Registro.php"</script>';
+}
+$ejecutar =mysqli_query($conexion, $query);
 
-
+if($ejecutar){
+    echo '<script language="javascript">
+    alert("Registro exitoso");window.location.href="../Registro.php"</script>';
+}else{
+    echo '<script language="javascript">
+    alert("intente de nuevo");window.location.href="../Registro.php"</script>';
+}
+mysqli_close($conexion);
 ?>
