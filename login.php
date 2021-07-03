af97ee5778a2b6b8c4293777fa8f81b1fcec769b
Freddy50309 <64513432+Freddy50309@users.noreply.github.com>
2021-07-03 18:58:08 -0500

    Update login.php
    Cambio de Fondo

--- login.php
+++ login.php
@@ -1,11 +1,12 @@
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
+
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <LINK REL="stylesheet" TYPE="text/css" HREF="css/estilo_login.css">
 </head>
+
 
 <body>
 
@@ -17,6 +18,7 @@
 
           <div class="login-form">
             <div class="sign-in-htm">
+
             <form action="logica/loguear.php" method="POST">
               <div class="group">
 
@@ -25,18 +27,21 @@
                  <br></br>
                  <br></br>
 
-                <label for="user" class="label">nombre</label>
+             <b>   <label for="user" class="label">Nombre</label>
                 <input id="user" style="color:#050505" name="usuario" type="text" class="input">
               </div>
               <div class="group">
                 <label for="pass" class="label">Apellido </label>
                 <input id="pass" style="color:#050505" name="clave" type="password" class="input" data-type="password">
+
               </div>
 
-
+              </b>
                 <div class="modal-footer">
+
                   <button id="envio" class="boton azul">INICIAR</button>
                   <a href="inicio.php" class="boton rojo">CANCELAR</a>
+
                 </div>
                 </form>
             </div>
