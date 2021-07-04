88aefc2e9b93714cec414902b2e2686cb235bc69
Freddy50309 <64513432+Freddy50309@users.noreply.github.com>
2021-07-03 20:03:26 -0500

    Update and rename Models/Numeros/Menu.php to Modelos/Numeros/Menu.php
    Modificación de fondo

--- Modelos/Numeros/Menu.php
+++ Modelos/Numeros/Menu.php
@@ -16,23 +16,23 @@ if(!isset($usuario)){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="estilos/EstiloMenu.css" />
-  <center><br>  <img src="Imagenes/Menu/numeros.png"  width="65%"
-     height="70">
+  <center><br>  
+  
 </head>
 <body> 
 
 <head>
-   
+<a href="../menu.php" style=" background: url(Imagenes/041-home.png) no-repeat center center;
+ background-size: 50%; height: 75px;
+  width: 110px;" class="boton1" ></a>
  <center>
- <br><br>
-    <a href="Seleccion.php" style=" background: url(Imagenes/Menu/btnInicio.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; background-size: 105%; height: 150px;  /* Alto del botón */
+ <br><br><br><br>
+    <a href="Seleccion.php" style=" background: url(Imagenes/Menu/btnInicio.png) /* Url de la imagen */ no-repeat center center /* Color del botón */; background-size: 105%; height: 150px;  /* Alto del botón */
   width: 150px;  /* Ancho del botón */"class="boton" ></a>
     <a href="../menu.php" style=" background: url(Imagenes/Menu/btnSalir.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; background-size: 105%; height: 80px;  /* Alto del botón */
   width: 100px;  /* Ancho del botón */"class="boton"></a> 
   
-<a href="../menu.php" style=" background: url(Imagenes/este.PNG) /* Url de la imagen */ no-repeat center center, 
-#62BC0F /* Color del botón */; background-size: 110%; height: 55px;  /* Alto del botón */
-  width: 110px;  /* Ancho del botón */"class="boton1" ></a>
+
    </center>
  
     </head>
