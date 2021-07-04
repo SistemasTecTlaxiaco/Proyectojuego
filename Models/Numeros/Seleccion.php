e6f5d11a7ffb59fdf46e50bc3c9276dd06b90a70
Freddy50309 <64513432+Freddy50309@users.noreply.github.com>
2021-07-03 20:09:20 -0500

    Add files via upload
    Corrección del fondo y estilos

---
+++ Models/Numeros/Seleccion.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+ <!--- <div class="contenedor">--->
+ <?php
+
+session_start();
+$usuario = $_SESSION['username'];
+
+if(!isset($usuario)){
+    header("location: ../../login.php");
+
+
+}
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="estilos/EstiloSeleccion.css" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimum-scale=1.0">
+
+   
+<a href="../menu.php" style=" background: url(Imagenes/041-home.png) no-repeat center center;
+ background-size: 50%; height: 75px;
+  width: 110px;"class="leekids" ></a>
+</head>
+
+<body>
+<section> 
+  
+<a href="Nivel1/Ejercicio1.php" style=" background: url(Imagenes/Menu/Uno1.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 125%; 
+height: 215px;  /* Alto del botón */ 
+ width: 225px;  /* Ancho del botón */"class="boton"></a>
+
+ <a href="Nivel2/Seleccion.php" style=" background: url(Imagenes/Menu/Dos2.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 125%; 
+height: 215px;  /* Alto del botón */ 
+ width: 225px;  /* Ancho del botón */"class="boton2"></a>
+
+<a href="Nivel3/Seleccion.php" style=" background: url(Imagenes/Menu/Tres3.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 125%; 
+height: 215px;  /* Alto del botón */ 
+ width: 225px;  /* Ancho del botón */"class="boton3"></a>
+
+<a href="Menu.php" style=" background: url(Imagenes/Menu/btnSalir.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; 
+background-size: 115%;
+ height: 90px;  /* Alto del botón */
+  width: 100px;  /* Ancho del botón */"class="boton4"></a> 
+</section>
+</body>
+</html>
