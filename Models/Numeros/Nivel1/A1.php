79926dba81010d6b9dfa97758cf3ba420242bfe0
Freddy50309 <64513432+Freddy50309@users.noreply.github.com>
2021-07-03 23:58:56 -0500

    Add files via upload
    Cambios en el fondo y estilo de los botones

---
+++ Models/Numeros/Nivel1/A1.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+ <!--- <div class="contenedor">--->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../estilos/EstiloA0.css" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimum-scale=1.0">
+  
+  <a href="../../menu.php" style=" background: url(../Imagenes/041-home.PNG) no-repeat center center;
+ background-size: 50%; height: 75px;
+  width: 110px;*/"class="leekids" ></a>
+  
+</head>
+<body><center>
+<video src="../video/numeros.mp4" width=60%  height=300 controls poster="../Imagenes/Nivel1/CancionVideo.png">
+</video></center>
+<a href="Ejercicio1.php" style=" background: url(../Imagenes/Menu/btnSalir.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 125%; 
+height: 90px;  /* Alto del botón */
+  width: 100px;  /* Ancho del botón */"class="boton"></a>
+
+
+</body>
+</html>
