0386942207bfd44d12bc69125bf9e3512e6fc68e
Freddy50309 <64513432+Freddy50309@users.noreply.github.com>
2021-07-04 00:00:16 -0500

    Add files via upload
    Cambio en el fondo y en estilo de  botones

---
+++ Models/Numeros/Nivel2/A3.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+ <!--- <div class="contenedor">--->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../estilos2/EstiloA3.css" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimum-scale=1.0">
+    
+    <a href="../../menu.php" style=" background: url(../Imagenes/041-home.PNG) no-repeat center center;
+ background-size: 50%; height: 75px;
+  width: 110px;"class="leekids" ></a>
+  
+<a href="Seleccion.php" style=" background: url(../Imagenes/Menu/btnSalir.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 125%; 
+height: 90px;  /* Alto del botón */
+  width: 100px;  /* Ancho del botón */"class="boton1"></a>
+  
+</head>
+<body>
+<section>
+
+<center>
+<a href="#" style=" background: url(../Imagenes/Nivel2/bocina.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 115%; 
+height: 140px;  /* Alto del botón */ 
+ width: 140px;  /* Ancho del botón */"class="boton"> </a>
+ <br>
+<img src="../Imagenes/Nivel2/L1.png"  width="12.2%" height="10%" />
+<img src="../Imagenes/Nivel2/L2.png"  width="12.1%" height="10%" />
+<img src="../Imagenes/Nivel2/L3.png"  width="12.7%" height="10%" />
+
+<a href="A3.1.php" style=" background: url(../Imagenes/Menu/flecha.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 125%; 
+height: 90px;  /* Alto del botón */
+  width: 100px;  /* Ancho del botón */"class="boton2"></a>
+
+</center>
+</section> 
+
+</body>
+</html>
---
+++ Models/Numeros/Nivel2/Seleccion.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../estilos2/EstiloSeleccion.css" />
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximun-scale=1.0, minimum-scale=1.0">
+   
+   <a href="../../menu.php" style=" background: url(../Imagenes/041-home.PNG) no-repeat center center;
+ background-size: 50%; height: 75px;
+  width: 110px; */"class="leekids" ></a>
+  
+</head>
+<body>
+<a href="../Seleccion.php" style=" background: url(../Imagenes/Menu/btnSalir.png) /* Url de la imagen */ no-repeat center center, #62BC0F /* Color del botón */; 
+background-size: 115%;
+ height: 90px;  /* Alto del botón */
+  width: 100px;  /* Ancho del botón */"class="boton4"></a> 
+<section>
+<a href="A1.php" style=" background: url(../Imagenes/Nivel2/Planilla.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 115%; 
+height: 215px;  /* Alto del botón */ 
+ width: 225px;  /* Ancho del botón */"class="boton"></a>
+
+<a href="A2.php" style=" background: url(../Imagenes/Nivel2/Etiquetas.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 115%; 
+height: 215px;  /* Alto del botón */ 
+ width: 225px;  /* Ancho del botón */"class="boton2"></a>
+
+<a href="A3.php" style=" background: url(../Imagenes/Nivel2/Escuchaa.png) /* Url de la imagen */ no-repeat center center 
+/* Color del botón */; background-size: 125%; 
+height: 215px;  /* Alto del botón */ 
+ width: 225px;  /* Ancho del botón */"class="boton3"></a>
+ 
+
+
+</section>
+</body>
+</html>
