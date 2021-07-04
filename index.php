c899c09860b417a29c3d6d75f046c260fe1ff6a5
Freddy50309 <64513432+Freddy50309@users.noreply.github.com>
2021-07-03 19:01:37 -0500

    Update index.php
    Cambio del Fondo

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 <!DOCTYPE html>
 <html lang="es">
 <head>
+
 	<meta charset="utf-8">
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <LINK REL="stylesheet" TYPE="text/css" HREF="css/estilo_index.css">
@@ -10,26 +12,11 @@
 
 <body>
 
-<div class="container">
 
-
-    <div class="login-wrap">
-        <div class="login-html">
-          <div class="login-form">
-            <div class="sign-in-htm">
-
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br>
              <div class="modal-footer">
-               <a href="inicio.php" class="boton verde">JUGAR</a>
-               <a href="#" class="boton rojo">SALIR</a>
+             <big><big> <big><big> <b> <a href="inicio.php" class="boton verde">JUGAR</a></b></big></big></big></big>
+             
              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-</div>
+
 </body>
 </html>
