60356025b425df3ab1dda63c02d3d084694e48f7
Freddy50309 <64513432+Freddy50309@users.noreply.github.com>
2021-07-03 19:40:33 -0500

    Update and rename Models/menu.php to Modelos/menu.php
    Cambio de fondo y modificación de estilos

--- Modelos/menu.php
+++ Modelos/menu.php
@@ -21,34 +21,34 @@ if(!isset($usuario)){
     <LINK REL="stylesheet" TYPE="text/css" HREF="estilo_menu.css">
 </head>
 
-<body>
+<body background="../css/Imagenes/niveles.png" > <!--style="background-size: cover;"-->
     <center>
         <h1>Bienvenido <?php echo $usuario;?></h1>
     </center>
-    <table width="50%">
+    <table width="80%">
         <tr>
             <td data-label="uno" width="100" height="100"> </td>
             <td data-label="dos" width="100" height="100">
-                <img src="Imagenes/voocales.png" width="300" height="150">
+                <img src="Imagenes/vocales.svg" width="350" height="150">
                 <div align="center"><a href="Vocales/vocales.php" class="boton amarillo">Jugar</a> </div>
             </td>
             <td data-label="tres" width="100" height="100"> </td>
-            <td data-label="cuatro" width="100" height="100"> <img src="Imagenes/numeros.jpg" width="300" height="150">
+            <td data-label="cuatro" width="100" height="100"> <img src="Imagenes/Numero.svg" width="350" height="150">
                 <div align="center"> <a href="Numeros/Menu.php" class="boton verde">Jugar</a> </div>
             </td>
             <td data-label="cinco" width="100" height="100"> </td>
         </tr>
         <tr>
-            <td data-label="uno" width="100" height="100">
-                <img src="Imagenes/letras.jpg" width="300" height="150">
+            <td data-label="uno" width="150" height="150">
+                <img src="Imagenes/letras.svg" width="350" height="180">
                 <div align="center"> <a href="Abecedario/index.html" class="boton azul">Jugar</a></div>
             </td>
             <td data-label="dos" width="100" height="100"> </td>
             <td data-label="tres" width="100" height="100">
-                <img src="Imagenes/figuras.jpg" width="300" height="150">
+                <img src="Imagenes/formas.svg" width="350" height="180">
                 <div align="center"> <a href="Figuras/Menu.php" class="boton violeta">Jugar</a></div>
             </td>
-            <td data-label="cuatro" width="100" height="100"> </td>
+            <td data-label="cuatro" width="100" height="-100"> </td>
             <td data-label="cinco" width="100" height="100">
                 
         </tr>
