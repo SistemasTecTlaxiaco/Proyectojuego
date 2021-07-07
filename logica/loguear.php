53d6f1013e82d584059d1a9aeedf13c32d214a07
Francisco <lazarojuarez.pa@gmail.com>
2021-07-07 13:20:58 -0500

    modificando loguear

--- logica/loguear.php
+++ logica/loguear.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+</head>
+</html>
+
 <?php
 
 require 'conexion.php';
@@ -19,7 +30,7 @@ if($array['contar']>0){
 }else{
     echo '<script language="javascript">alert("Error de autentificacion");window.location.href="../login.php"</script>';
 }
-
+//echo '<script language="javascript">alert("Error de autentificacion");window.location.href="../login.php"</script>';
 
 
 ?>
